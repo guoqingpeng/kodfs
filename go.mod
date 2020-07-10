0ade307c04da532a43272523d5cd1dd6c63531ee
kakuseihou <xiaogqp@163.com>
2020-07-10 15:50:07 +0800

    开始编写分布式对象存储系统

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/guoqingpeng/kodfs
+
+go 1.13
+
+require github.com/guoqingpeng/oney v0.0.0-20200219003741-2dc284b9a7b7 // indirect
---
+++ kodfs.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/guoqingpeng/oney/byy"
+	)
+
+
+func main() {
+
+	fmt.Print("分布式对象存储系统开始服务")
+	fmt.Println()
+	byy.Bygass()
+
+}
