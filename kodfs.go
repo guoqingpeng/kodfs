bbadcfe686dc8b1ea10bb4d3eff0a61c759ab96c
kakuseihou <xiaogqp@163.com>
2020-07-10 17:00:50 +0800

    提交住配置文件

--- kodfs.go
+++ kodfs.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/guoqingpeng/kodfs/kodfs_config"
-	)
+)
 
+//set up system args accept from the console
+var (
+	configRoot *string = flag.String("c", "./conf", "provide the config file path")
+	logRoot    *string = flag.String("l", "./log", "provide the log file path")
+)
 
 func main() {
 
-	fmt.Print("分布式对象存储系统开始服务")
-	fmt.Println()
+	fmt.Println("可牛了。。。分布式对象存储系统开始服务")
 	fmt.Println(kodfs_config.Config())
+
+	//start	to parse the args from inputs
+	flag.Parse()
+	fmt.Println(*configRoot)
+	fmt.Println(*logRoot)
+	//end parser
 
 }
