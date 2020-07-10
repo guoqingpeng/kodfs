48e509af161b290385ef76b33f1f3c3da10a8cb1
kakuseihou <xiaogqp@163.com>
2020-07-10 18:09:15 +0800

    解析主配置文件

--- kodfs.go
+++ kodfs.go
@@ -3,27 +3,30 @@ package main
 import (
 	"flag"
 	"fmt"
-
 	"github.com/guoqingpeng/kodfs/kodfs_config"
 )
 
 //set up system args only accept from the console
 
 var (
-	configRoot *string = flag.String("c", "./conf", "provide the config file path")
+	configRoot *string = flag.String("c", "/conf", "provide the config file path")
 	logRoot    *string = flag.String("l", "./log", "provide the log file path")
 )
 
 func main() {
 
 	fmt.Println("可牛了。。。分布式对象存储系统开始服务")
-	fmt.Println(kodfs_config.Config())
 
 	//step1 start	to parse the args from inputs
 	flag.Parse() //别忘记了写这个
-
-	fmt.Println(*configRoot)
-	fmt.Println(*logRoot)
 	//end parser
+
+	//step2 解析主配置文件
+	confPath := *configRoot + "/kodfs.cfg"
+	fmt.Println(confPath)
+
+	cfg := kodfs_config.NewKodsConfig()
+
+	cfg.ParseConfig(confPath)
 
 }
--- kodfs_config/config.go
+++ kodfs_config/config.go
@@ -1,7 +1,55 @@
 package kodfs_config
 
-func Config() string {
+import (
+	"fmt"
+	"os"
+)
 
-	return "this is a config"
+type KodfsConfig struct {
+	httpPort int
 
+	httpsPort int
+
+	MaxCups int
+}
+
+//init a object
+func NewKodsConfig() *KodfsConfig {
+
+	k := new(KodfsConfig)
+
+	k.SetDefaultConfig()
+
+	return k
+}
+
+// use defalut value if config file not set
+func (cfg *KodfsConfig) SetDefaultConfig() {
+
+	cfg.httpPort = 8880
+
+	cfg.httpsPort = 4443
+
+	cfg.MaxCups = 4
+
+}
+
+//parse config file for real
+func (cfg *KodfsConfig) ParseConfig(confPath string) {
+
+	configFile, error := os.Open(confPath)
+
+	if error != nil {
+		fmt.Println("发生异常了")
+		fmt.Println(error)
+	}
+
+	fmt.Println(configFile)
+
+}
+
+//获取http端口
+func (cfg *KodfsConfig) GetHttpPort() int {
+
+	return cfg.httpPort
 }
