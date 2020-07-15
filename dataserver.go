b0aa42b3d502a7f5dbb6a6f84144599ff187c7cc
kakuseihou <xiaogqp@163.com>
2020-07-15 14:34:12 +0800

    添加heartbeat消息

--- dataserver.go
+++ dataserver.go
@@ -5,41 +5,46 @@ import (
 	"fmt"
 	"github.com/guoqingpeng/kodfs/kodfs_dataserver"
 	"net"
+	"time"
 )
 
 func main() {
+	for {
 
-	serverAddr := "192.168.6.20:52255"
-	tcpAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
-	if err != nil {
-		fmt.Println("Resolve TCPAddr error", err)
+		serverAddr := "192.168.6.20:52255"
+		tcpAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
+
+		if err != nil {
+			fmt.Println("Resolve TCPAddr error", err)
+		}
+		conn, err := net.DialTCP("tcp", nil, tcpAddr)
+
+		if err != nil {
+			fmt.Println("connect server error", err)
+		}
+
+		ds := kodfs_dataserver.NewDataNode()
+		ds.Dataserver_ip = "192.168.6.20"
+		ds.Dataserver_port = 55255
+		ds.Dataserver_name = "kaku1"
+		ds.Data_dir = "./data/"
+		ds.Left_capacity = 34 * 1025
+		ds.Total_capacity = 2024 * 1024
+		ds.Server_status = 0
+		var dsBytes, _ = json.Marshal(ds)
+		conn.Write(dsBytes)
+		recv(conn)
+		conn.Close()
+		time.Sleep(time.Millisecond * 20)
 	}
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	defer conn.Close()
-	if err != nil {
-		fmt.Println("connect server error", err)
-	}
-
-	ds := kodfs_dataserver.NewDataNode()
-	ds.Dataserver_ip = "192.168.6.20"
-	ds.Dataserver_port = 55255
-	ds.Dataserver_name = "kaku1"
-	ds.Data_dir = "./data/"
-	ds.Left_capacity = 34 * 1025
-	ds.Total_capacity = 2024 * 1024
-	ds.Server_status = 0
-	var dsBytes, _ = json.Marshal(ds)
-	fmt.Println(string((dsBytes)))
-
-	conn.Write(dsBytes)
-	recv(conn)
 }
 
 func recv(conn net.Conn) {
 	buffer := make([]byte, 1024)
 	n, err := conn.Read(buffer)
+
 	if err == nil {
 		fmt.Println("read message from server:" + string(buffer[:n]))
-		fmt.Println("Message len:", n)
 	}
+
 }
